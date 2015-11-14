771aa980a99f9956eff4ef73b9551705a4aa6344
Jingning <zhangjn@bu.edu>
2015-11-13 20:05:55 -0500

    Starting making the controller

---
+++ MulticycleCPU/Controller.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    20:03:33 11/13/2015 
+// Design Name: 
+// Module Name:    Controller 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Controller(CtrlbitStream);
+
+
+endmodule
