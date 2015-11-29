c6b0808b2dec64cf9a57e7a63181ed181a25fcd8
Jingning <zhangjn@bu.edu>
2015-11-29 00:06:15 -0500

    fix the problem PCsource

--- MulticycleCPU/Datapath.v
+++ MulticycleCPU/Datapath.v
@@ -113,7 +113,7 @@ default: assign Branch_flag = 0;
 endcase
 end
 
-assign Branchornot = (Branch_flag)?PC:IMM32;
+assign Branchornot = (Branch_flag)?IncrementPC:IMM32;
 threemux32 pcsrc(IncrementPC,Branchornot,JMPaddress,PCSrc,NextPC);
 
 //Dmem
